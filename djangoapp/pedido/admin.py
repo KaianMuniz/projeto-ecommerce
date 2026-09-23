33e1d003aa9b8d29d11123caa1b5beaf5f68995f
Kaian Muniz <kaianmuniz5@gmail.com>
2026-09-23 01:07:13 -0300

    create pedido and itempedido models

--- djangoapp/pedido/admin.py
+++ djangoapp/pedido/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Pedido, ItemPedido
+
+
+class ItemPedidoInline(admin.TabularInline):
+    model = ItemPedido
+    extra = 1
+
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'usuario',
+        'total',
+        'status',
+    )
+
+    list_filter = (
+        'status',
+    )
+
+    search_fields = (
+        'id',
+        'usuario__username',
+    )
+
+    inlines = [
+        ItemPedidoInline,
+    ]
+
+
+@admin.register(ItemPedido)
+class ItemPedidoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'pedido',
+        'produto',
+        'variacao',
+        'preco',
+        'quantidade',
+    )
+
+    search_fields = (
+        'produto',
+        'variacao',
+    )
